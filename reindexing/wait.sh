c07811f8a61b6d004dbdedffd621018e2abee37d
Angel Borroy <angel.borroy@alfresco.com>
2021-08-20 17:03:22 +0200

    first commit

--- reindexing/wait.sh
+++ reindexing/wait.sh
@@ -3,9 +3,8 @@
 TIMEOUT="${TIMEOUT:-600}"
 seconds=0
 
-echo 'Waiting up to' $TIMEOUT 'seconds for HTTP 200 from' $URL
+echo 'Waiting up to' $TIMEOUT 'seconds for HTTP 200'
 until [ "$seconds" -gt "$TIMEOUT" ] || $(curl --output /dev/null --silent --max-time $TIMEOUT --fail http://alfresco:8080/alfresco/s/api/server); do
-  printf '.'
   sleep 30
   seconds=$((seconds+30))
 done
@@ -13,6 +12,6 @@ done
 if [ "$seconds" -lt "$TIMEOUT" ]; then
   echo 'OK'
 else
-  echo "ERROR: Timed out wating for HTTP 200 from" $URL >&2
+  echo "ERROR: Timed out wating for HTTP 200" >&2
   exit 1
 fi
